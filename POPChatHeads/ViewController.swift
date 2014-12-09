4cafbc8682d24d830564a5107f4706730f81fc90
Fábio Bernardo <mantyftw@gmail.com>
2014-12-09 00:44:16 +0000

    Initial Commit

---
+++ POPChatHeads/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  POPChatHeads
+//
+//  Created by Fábio Bernardo on 09/12/14.
+//  Copyright (c) 2014 fbernardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
